3f2a40a14da3da7e33141007fee4282555703018
chuanshuozhongdeshen <chuanshuozhongdeshen@outlook.com>
2025-12-19 23:46:10 +0800

    init project

---
+++ src/main/java/com/moon/ees/EesApplication.java
@@ -0,0 +1,13 @@
+package com.moon.ees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EesApplication.class, args);
+    }
+
+}
