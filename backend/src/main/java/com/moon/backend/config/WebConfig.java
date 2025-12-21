6dcb392fca85f476795b4d3df629bd350c5a55ff
chuanshuozhongdeshen <chuanshuozhongdeshen@outlook.com>
2025-12-21 22:41:39 +0800

    chore:3

--- backend/src/main/java/com/moon/backend/config/WebConfig.java
+++ backend/src/main/java/com/moon/backend/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig {
@@ -19,7 +20,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
